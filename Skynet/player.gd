abf70eb191f8a0b0657b964f111568c6bdd1d436
eiganesteknolab <nav@eiganesteknolab.com>
2025-04-24 19:38:00 +0200

    Changes

---
+++ Skynet/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -400.0
+
+@onready var sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+		sprite.scale.x = -1 if direction < 0 else 1
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
